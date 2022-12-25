6604a1a424ebcfd8d1bd17a26f4da373726b2365
SangTran-127 <tranquangsang12.7@gmail.com>
2022-12-26 02:03:26 +0700

    setup bot

---
+++ index.js
@@ -0,0 +1,19 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+// setup
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
